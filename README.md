6319000fc4d0e3f28843bde6555465ce19bbf9da
Alex Shumilov <ashumilov@pivotal.io>
2019-03-25 12:33:37 -0400

    Initial setup.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Minimum Setup for Spring Boot application.
---
+++ src/main/java/io/microsamples/hybrid/AddedBootApp.java
@@ -0,0 +1,11 @@
+package io.microsamples.hybrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddedBootApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AddedBootApp.class, args);
+    }
+}
