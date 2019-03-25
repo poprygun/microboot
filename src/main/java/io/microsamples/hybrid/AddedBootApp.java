b6a011960e46a742e839193cdee1f2be4da14119
Alex Shumilov <ashumilov@pivotal.io>
2019-03-25 12:42:15 -0400

    Added annotation to specify packages to scan.

--- src/main/java/io/microsamples/hybrid/AddedBootApp.java
+++ src/main/java/io/microsamples/hybrid/AddedBootApp.java
@@ -2,8 +2,9 @@ package io.microsamples.hybrid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"io.microsamples.hybrid"})
 public class AddedBootApp {
     public static void main(String[] args) {
         SpringApplication.run(AddedBootApp.class, args);
